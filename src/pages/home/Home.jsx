75a60a3c15d99a94d11eabc42ba10a4a2cc62717
Caitlin <caitlinramsey@outlook.com>
2023-11-15 19:50:48 -0500

    added some animation to homepage text

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,8 +13,8 @@ export default function Home() {
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
-                            <h2 class="hero_title">Welcome to Velaris Stables</h2>
-                            <p class="hero_desc">We are a boarding, training, and lesson facility located in Raleigh, North Carolina.</p>
+                            <h2 className="hero_title">Welcome to Velaris Stables</h2>
+                            <p className="hero_desc">We are a boarding, training, and lesson facility located in Raleigh, North Carolina.</p>
                         </div>
                     </div>
                 </div>
