9bd74a69e6880789a6a0daff98456653891ed5d1
Caitlin <caitlinramsey@outlook.com>
2023-11-18 15:59:31 -0500

    working on bug fixes

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import "./navigation.css";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -18,41 +18,41 @@ function Navigation() {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
-              <Nav.Link
+              <Link
                 to="/"
                 onClick={() => handlePageChange("Home")}
                 className={currentPage === "Home" ? "active" : "nav-link"}
               >
                 Home
-              </Nav.Link>
-              <Nav.Link
+              </Link>
+              <Link
                 to="/About"
                 onClick={() => handlePageChange("About")}
                 className={currentPage === "About" ? "active" : "nav-link"}
               >
                 About
-              </Nav.Link>
-              <Nav.Link
+              </Link>
+              <Link
                 to="/Boarding"
                 onClick={() => handlePageChange("Boarding")}
                 className={currentPage === "Boarding" ? "active" : "nav-link"}
               >
                 Boarding
-              </Nav.Link>
-              <Nav.Link
+              </Link>
+              <Link
                 to="/Lessons"
                 onClick={() => handlePageChange("Lessons")}
                 className={currentPage === "Lessons" ? "active" : "nav-link"}
               >
                 Lessons
-              </Nav.Link>
-              <Nav.Link
+              </Link>
+              <Link
                 to="/Contact"
                 onClick={() => handlePageChange("Contact")}
                 className={currentPage === "Contact" ? "active" : "nav-link"}
               >
                 Contact
-              </Nav.Link>
+              </Link>
           </Nav>
         </Navbar.Collapse>
 
