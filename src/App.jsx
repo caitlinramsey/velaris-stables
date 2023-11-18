13a5bf7cd8f1ed24881557c992729f6da0e4e41c
Caitlin <caitlinramsey@outlook.com>
2023-11-18 15:28:46 -0500

    fixed build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Navigation from './components/nav/Nav';
+import Nav from './components/nav/Nav';
 import Header from './components/header/Header';
 // import Footer from './components/footer/Footer'
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     <Header />
-      <Navigation />
+      <Nav />
       <main>
         <Outlet />
         {/* <Footer /> */}
