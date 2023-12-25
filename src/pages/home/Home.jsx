1d16dcfa1b5e39649404ffe76dde3d7cc350846d
Caitlin <caitlinramsey@outlook.com>
2023-12-24 20:31:46 -0500

    Changed hero image

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import './home.css'
-import background from '../../../images/svenhero.jpg'
+import background from '../../../images/sven-right-no-halter.png'
 
 function Home() {
     return (
@@ -9,13 +9,12 @@ function Home() {
                 backgroundRepeat: 'no-repeat',
                 height: '100vh',
                 backgroundSize: 'cover',
-                opacity: '.9',
                 position: 'relative',
             }}>
                 <section class="bgimage" id="home">
                     <div class="container-fluid">
                         <div class="row">
-                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5 hero-text">
                                 <h2 className="hero_title">Welcome to Velaris Stables</h2>
                                 <p className="hero_desc">We are a boarding, training, and lesson facility located in Raleigh, North Carolina.</p>
                             </div>
