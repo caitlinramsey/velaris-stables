b9ae9a1f1164ef3636cf5ff18f15f9a1fb0bbc00
Caitlin <caitlinramsey@outlook.com>
2023-12-25 17:22:43 -0500

    More tweaking styles and changed out some images

--- src/pages/lessons/Lessons.jsx
+++ src/pages/lessons/Lessons.jsx
@@ -21,13 +21,13 @@ function Lessons() {
                     <div className="lesson-desc col-lg-12 text-center">
                         <p> We offer lessons ranging from beginner to advanced in multiple disciplines. We offer lessons in hunters, jumpers, dressage, cross country, trail, and general western.
                         </p>
-                        <div className="row mt-3">
+                        <div className="row mt-3 mb-4">
                             <p> In order to determine who you should work with and what your riding level is, we require an initial one-on-one lesson where one of our trainers can evaluate your riding ability. You will need to show up 15 minutes before your lesson to groom and tack up your horse.
                             </p>
                             <p>Don't have your own horse? No problem! We have a wide range of lovely lesson horses who can suit many different riders' needs.
                             </p>
                         </div>
-                        <div className="lesson-pricing-section">
+                        <div className="lesson-pricing-section mb-4">
                             <h1 className="lesson-pricing fw-bold">Lesson Pricing</h1>
                             <div className="lesson-prices">
                                 <p><span className="fw-bold">30</span> minute private lesson -- <span className="fw-bold">$40</span></p>
@@ -38,7 +38,7 @@ function Lessons() {
                         </div>
 
                         <h1 className="meet-trainers fw-bold"> Meet the Trainers</h1>
-                        <div className="col-lg-10 trainer-info">
+                        <div className="col-lg-8 trainer-info">
                             <div style={{
                                 backgroundImage: `url(${me})`,
                                 backgroundRepeat: 'no-repeat',
@@ -49,12 +49,12 @@ function Lessons() {
                                 margin: '0 auto',
                             }}></div>
                             <h4 className="caitlin-ramsey">Caitlin Ramsey</h4>
-                            <p>Caitlin rode her first horse at 2 years old and started taking lessons at 6 years old. She has experience with hunters, jumpers, eventing, dressage, driving, and a small bit in reining.
+                            <p>Caitlin rode her first horse at 2 years old and started taking lessons at 6 years old. She has experience with hunters, jumpers, eventing, dressage, driving, and a small bit in reining. She teaches beginner and some intermediate lessons while spending most of her time working with her young horse, Sven.
                             </p>
                         </div>
                     </div>
                     <div id="lesson-horses" className="row text-center">
-                        <h1 id="meet-our-horses" className="text-center fw-bold col-12">Meet Our Horses</h1>
+                        <h1 id="meet-our-horses" className="text-center fw-bold col-12 pb-3">Meet Our Lesson Horses</h1>
 
                         {[
                             { image: sven, name: 'Sven', description: 'Sven is a 3 year old, 13.3 hand, Norwegian Fjord gelding who is not part of the lesson program yet, but will be once he is old enough.' },
