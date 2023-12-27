2dbde03eb78835d5da5bb968bd6d77bf8e90d861
Caitlin <caitlinramsey@outlook.com>
2023-12-26 22:30:47 -0500

    Changed out benny photo on lesson page and worked on grammer/adding to about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -20,19 +20,20 @@ function About() {
                         }}></div>
                     </div>
 
-                    <div className="col-lg-8">
-                        <p> Velaris Stables is a 100 acre facility with some wonderful amenities for you and your horse. The facility boasts a state of the art 100'x200' indoor arena with rubber footing as well as a 200'x300' outdoor arena with the same footing. Both arenas have full jump courses that are changed weekly and all weather footing.
+                    <div className="col-lg-8 about-desc">
+                        <p> Velaris Stables is a <span className="fw-bold">100</span> acre facility with some wonderful amenities for both you and your horse. The facility boasts a state of the art <span className="fw-bold">100'x200'</span> indoor arena with rubber footing as well as a <span className="fw-bold">200'x300'</span> outdoor arena with the same footing. Both arenas have full jump courses that are changed weekly have all weather footing.
                         </p>
                         <p> For those not interested in jumping, we have a regulation dressage arena with a speaker system you can connect your phone to so you can practice your freestyle.
                         </p>
-                        <p> Other amenities include a full cross country course with a water feature, miles of trails, a trail obstacle course, and a hot walker.
+                        <p> Other amenities include a full cross country course with a water feature, miles of trails, a covered <span className="fw-bold">80'</span> round pen with wooden sides, a trail obstacle course, and a hot walker.
                         </p>
-                        <p> Inside the main barn, we have 12'x12' and 14'x14' matted stalls with automatic waterers. The barn has 8 grooming stalls as well as 2 H/C wash stalls with infrared heaters. We have 3 tack rooms; one for boarders, one for lessons, and one for our owner and trainers. The tack rooms are heated and air conditioned with furniture, a kitchenette, and a lovely view of the indoor arena. We also offer trailer parking on concrete with access to water to make it easy to wash your trailer.
+                        <p> Inside the main barn, we have <span className="fw-bold">12'x12'</span> and <span className="fw-bold">14'x14'</span> matted stalls with automatic waterers. The barn has <span className="fw-bold">8</span> grooming stalls as well as <span className="fw-bold">2</span> H/C wash stalls with infrared heaters. We have <span className="fw-bold">3</span> tack rooms; one for boarders, one for lessons, and one for our owner and trainers. The tack rooms are heated and air conditioned with furniture, a kitchenette, a bathroom, and a lovely view of the indoor arena. We also offer trailer parking on concrete with access to water to make it easy to wash your trailer.
                         </p>
-                        <p> We will be offering a show series on site as well as clinics year round.
+                        <p> We will be offering a show series on site, hunter paces, and clinics year round.
                         </p>
                     </div>
-                    <div className="row mt-3">
+                    <hr style={{ border: '2px solid black', margin: '20px 0' }} />
+                    <div className="row mt-3 key-features-title">
                         <p>To highlight the <span className="fw-bold">key features</span> of our facility:
                         </p>
                         <div className="col-md-3 key-features fw-bold">
@@ -41,12 +42,14 @@ function About() {
                                 <li>200' x 300' outdoor arena</li>
                                 <li>20 x 60 meter dressage arena</li>
                                 <li>Full cross country course</li>
+                                <li>Covered round pen</li>
                                 <li>Miles of trails</li>
                                 <li>Trail obstacle course</li>
                                 <li>H/C wash stalls</li>
                                 <li>Heated and AC tack rooms</li>
                                 <li>Trailer parking</li>
                                 <li>Shows and clinics onsite</li>
+                                <li>Hot walker</li>
                             </ul>
                         </div>
                         <div className="col-lg-4 indoor">
@@ -69,7 +72,8 @@ function About() {
                         }}></div>
                     </div>
                     </div>
-                    <div className="row mt-3 text-center">
+                    <hr style={{ border: '2px solid black', margin: '20px 0' }} />
+                    <div className="row mt-3 text-center fs-4">
                         <p> We'd love to give you a tour of the facility and show what we have to offer you and your equine. Please navigate to the <Link to='/Contact' className='about-contact'>Contact Page</Link> to find out how to reach out to us and schedule a time!
                         </p>
                     </div>
--- src/pages/lessons/Lessons.jsx
+++ src/pages/lessons/Lessons.jsx
@@ -4,10 +4,10 @@ import './lessons.css';
 import me from '../../../images/me-hershey.jpg';
 import sven from '../../../images/lesson-horses/sven-front-no-halter.png';
 import hershey from '../../../images/lesson-horses/hershey.jpg';
-import benny from '../../../images/lesson-horses/benny.jpg';
 import freddie from '../../../images/lesson-horses/freddie.jpg';
 import zippy from '../../../images/lesson-horses/zippy.jpg';
 import linus from '../../../images/lesson-horses/linus.jpg';
+import bennyXc from '../../../images/lesson-horses/benny-xc.jpg';
 
 function Lessons() {
     return (
@@ -48,7 +48,7 @@ function Lessons() {
                                 overflow: 'hidden',
                                 margin: '0 auto',
                             }}></div>
-                            <h4 className="caitlin-ramsey">Caitlin Ramsey</h4>
+                            <h4 className="caitlin-ramsey fw-bold">Caitlin Ramsey</h4>
                             <p>Caitlin rode her first horse at 2 years old and started taking lessons at 6 years old. She has experience with hunters, jumpers, eventing, dressage, driving, and a small bit in reining. She teaches beginner and some intermediate lessons while spending most of her time working with her young horse, Sven.
                             </p>
                         </div>
@@ -59,7 +59,7 @@ function Lessons() {
                         {[
                             { image: sven, name: 'Sven', description: 'Sven is a 3 year old, 13.3 hand, Norwegian Fjord gelding who is not part of the lesson program yet, but will be once he is old enough.' },
                             { image: hershey, name: 'Hershey', description: 'Hershey is a 21 year old, 13.2 hand Welsh pony gelding. He loves jumpers and is very quick and handy.' },
-                            { image: benny, name: 'Benny', description: 'Benny is a 15 year old, 16 hand American Warmblood gelding. Benny is great for hunters, jumpers, and eventing. He is happy as long as there is a jump infront of him.' },
+                            { image: bennyXc, name: 'Benny', description: 'Benny is a 15 year old, 16 hand American Warmblood gelding. Benny is great for hunters, jumpers, and eventing. He is happy as long as there is a jump infront of him.' },
                             { image: freddie, name: 'Freddie', description: 'Freddie is a Quarter Horse gelding who was a pony horse on the racetrack. He is loving his second career learning to jump.' },
                             { image: zippy, name: 'Zippy', description: 'Zippy is a 15.2 hand, American Paint mare who has mainly done trail riding but is loving learning to jump and learning dressage.' },
                             { image: linus, name: 'Linus', description: 'Linus is a 4 year old, 14 hand, Appaloosa gelding. He is still young and learning but is very safe and quiet.' },
