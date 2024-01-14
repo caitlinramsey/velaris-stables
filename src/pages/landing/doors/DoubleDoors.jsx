4b29c628ab4fde7a8a0b2da9421b3e8ac22ff3c9
Caitlin <caitlinramsey@outlook.com>
2024-01-14 00:22:47 -0500

    Working on transition time

--- src/pages/landing/doors/DoubleDoors.jsx
+++ src/pages/landing/doors/DoubleDoors.jsx
@@ -21,7 +21,7 @@ const DoubleDoors = () => {
                 });
 
                 window.location.href = 'home';
-            }, 2000);
+            }, 6000);
         }
     };
 
@@ -45,16 +45,16 @@ const DoubleDoors = () => {
 
     return (
         <div className={`double-door-container ${isOpen ? 'open' : ''}`}>
-            <div className="door left-door"></div>
+            <div className='door left-door'></div>
             <div className='center-aisle'></div>
-            <div className="door right-door"></div>
+            <div className='door right-door'></div>
             <div className='enter-container'>
-                <a className='button home-link pt-5' href="home" rel="noopener noreferrer">
-                    <div className="button__line"></div>
-                    <div className="button__line"></div>
-                    <span className="button__text" onClick={handleToggle}>Enter</span>
-                    <div className="button__drow1"></div>
-                    <div className="button__drow2"></div>
+                <a className='button home-link pt-5' href='home' rel='noopener noreferrer'>
+                    <div className='button__line'></div>
+                    <div className='button__line'></div>
+                    <span className='button__text' onClick={handleToggle}>Enter</span>
+                    <div className='button__drow1'></div>
+                    <div className='button__drow2'></div>
                 </a>
             </div>
         </div>
